9204398ab2822c9f94163564ae7a76f334bd44d2
kaxadlec <kaxadlec@gmail.com>
2023-01-12 16:20:19 +0900

    countdown 02

--- day 01.py
+++ day 01.py
@@ -1,6 +1,11 @@
-# (100°F − 32) × 5/9 = 37.778°C
+# 교재 countdown 예제
+# for countdown in 5, 4, 3, 2, 1, "hey!":
+#     print(countdown)
+# print('프로그램 종료')   컨트롤슬래시로 주석처리함
 
-fahrenheit = float(input('화씨 온도 : '))
-celsius = (fahrenheit - 32.0) * (5.0/9.0)
-print(celsius)
-print(f'화씨온도 {fahrenheit}도는 섭씨온도 {celsius}입니다.')
+countdown_list = [5, 4, 3, 2, 1, "hey!"]
+for countdown in countdown_list:
+    print(countdown)
+print(countdown_list[3]) # 2가 출력될 것
+print(countdown_list[-1]) # hey! 출력
+print('프로그램 종료')
