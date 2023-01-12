1e309e966f22046f7a36e9d960387c9ff95f0a4c
kaxadlec <kaxadlec@gmail.com>
2023-01-12 16:24:39 +0900

    dictionary 03

--- day 01.py
+++ day 01.py
@@ -1,11 +1,16 @@
-# 교재 countdown 예제
-# for countdown in 5, 4, 3, 2, 1, "hey!":
-#     print(countdown)
-# print('프로그램 종료')   컨트롤슬래시로 주석처리함
-
-countdown_list = [5, 4, 3, 2, 1, "hey!"]
-for countdown in countdown_list:
-    print(countdown)
-print(countdown_list[3]) # 2가 출력될 것
-print(countdown_list[-1]) # hey! 출력
-print('프로그램 종료')
+# 교재 dictionary 예제
+subjects = {
+        '국어' : '90',
+        '수학' : '100',
+        '영어' : '80',
+ }
+student = '홍길동'
+subject = '수학'
+# 옛날 방법
+print(student, '학생의', subject, '과목성적은', subjects['수학'], '입니다')
+print("%s 학생의 %s 과목성적은 %s입니다" % (student, subject, subjects[subject]))
+# 모던 방법 (format 함수 활용)  0,1,2 포지셔닝 활용 가능, 숫자 굳이 안써도 순서대로 되긴함
+print("{2} 학생의 {1} 과목성적은 {0} 입니다".format(student, subject, subjects[subject]))
+print("{} 학생의 {} 과목성적은 {} 입니다".format(student, subject, subjects[subject]))
+# 스트링 방식 활용
+print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다.')
