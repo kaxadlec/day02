c5126e293a5571a487c630c09815727f3ceb77f0
kaxadlec <kaxadlec@gmail.com>
2023-01-13 15:22:43 +0900

    day02 chapter4

--- day02/day02.py
+++ day02/day02.py
@@ -1,14 +1,22 @@
-#number = 0b10011010
-#number = 0x9A
-#number = 0o232
-#print(number)
+# 챕터 4
+a=[]
+print(bool(a))
+a.append(5)   #추가
+print(bool(a))
+print(bool(set()))
+print(bool(dict()))
+print(bool("aa"))
 
-number = 154
-#print(bin(number))
-#print(hex(number))
-#print(oct(number))
+vowels = 'aeiou'
+letter ='x'
+if letter not in vowels:   # in 키워드 되게 유용
+    print("안 들어감")
 
-print(ord(" "))   # 아스키코드 space  ->  십진수 32
-print(hex(ord(" ")))   #  아스키코드 space -> 16진수 20
-print(ord("1"))
-
+limits = 20
+tweets = "pass" * 6
+diff = limits - len(tweets)
+if diff >= 0:
+# if diff := limits - len(tweets) >= 0:
+    print(tweets)
+else:
+    print(f'제한 글자 수 {abs(diff)} 초과')
